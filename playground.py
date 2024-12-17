7da1dcf85fa3e768f1c76fb17d6e101ed3cdba2d
BenediktHeld <benedikt.bh75@t-online.de>
2024-12-17 10:20:37 -0500

    Update playground.py

--- playground.py
+++ playground.py
@@ -5,3 +5,13 @@ def test(*args):
     return sum
 
 print(test(1,2,3,4,5,6,7,8,9))
+
+
+class Car:
+    def __init__(self,**kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+my_car = Car(make="BMW",model="XM")
+print(my_car.make)
+print(my_car.model)
